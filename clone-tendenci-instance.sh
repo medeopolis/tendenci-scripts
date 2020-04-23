568c4a3b30dd7abf23e9444beaead88c98e00946
Karl Goetz <karl@kgoetz.id.au>
2020-04-23 20:48:14 +1000

    Update rebuild script
    This features a number of changes designed to make the scripts output better for the user.
    
    * The entire test environment is mirrored to a new parent directory (by default
      tendenci_test) retaining the original folder names within
    * Other files (ie systemd unit name) are also controlled this way. Between these
      two changes building test/dev based on prod should be easier
    * More variables added to control behaviour
    * More settings changed in conf/settings.py
    * Settings are all updated via sed. This means changes are in place and
      configuration doesn't end up catted to the bottom of the file
    
    There are a couple of other changes:
    * nginx configuration is no longer updated. This is because dancing around the
      admins choices and the certbot automagic on top wasn't worth the effort.

--- clone-tendenci-instance.sh
+++ clone-tendenci-instance.sh
@@ -1,31 +1,34 @@
 #!/bin/bash -x
 
-# Variables
-website_path_prod=/srv/tendenci/website
-website_path_dev=/srv/tendenci/devsite
+### Variables
 
-nginx_config_prod=/etc/nginx/sites-available/tendenci
-nginx_config_dev=/etc/nginx/sites-available/tendenci-dev
+# project path requires underscore not dash due to python
+project_path_prod=/srv/tendenci
+project_path_dev=/srv/tendenci_test
+
+# Suffix for files for dev (pid, logs, ...)
+file_suffix_dev='-test'
+
+website_path_prod=$project_path_prod/website
+website_path_dev=$project_path_dev/website
 
 service_path_prod=/etc/systemd/system/tendenci.service
-service_path_dev=/etc/systemd/system/tendenci-dev.service
+service_path_dev=/etc/systemd/system/tendenci$file_suffix_dev.service
 
-env_path_prod=/srv/tendenci/environment
-env_path_dev=/srv/tendenci/devenv
+env_path_prod=$project_path_prod/environment
+env_path_dev=$project_path_dev/environment
+
+log_path_prod=$project_path_prod/logs
+log_path_dev=$project_path_dev/logs
 
 db_name_prod='tendenci_prod'
-db_name_dev='tendenci_dev'
+db_name_dev='tendenci_test'
 
 cd /
 
-# Sync nginx config from prod to dev 
-cp $nginx_config_prod $nginx_config_dev
-if ! [ 0 -eq $? ]; then echo "$nginx_config_dev wasn't created."; exit 1; fi
-
-# Update references
-sed -i -e "s|$website_path_prod|$website_path_dev|g" $nginx_config_dev
-sed -i -e "s|127.0.0.1:8000|127.0.0.1:8080|" $nginx_config_dev
-sed -i -e "s|listen 443|listen 8443|" $nginx_config_dev
+# Make the log path for the dev env with its parents. Environment and site have
+# their dirs created mid script.
+mkdir -p $log_path_dev
 
 service nginx reload
 
@@ -33,14 +36,15 @@ service nginx reload
 cp $service_path_prod $service_path_dev
 if ! [ 0 -eq $? ]; then echo "$service_path_dev wasn't created."; exit 1; fi
 
-# Update references
+# Update references in service file
 sed -i -e "s|$website_path_prod|$website_path_dev|g" $service_path_dev
-sed -i -e "s|tendenci.pid|tendenci-dev.pid|g" $service_path_dev
+sed -i -e "s|tendenci.pid|tendenci$file_suffix_dev.pid|g" $service_path_dev
 sed -i -e "s|$env_path_prod|$env_path_dev|g" $service_path_dev
 sed -i -e "s|127.0.0.1:8000|127.0.0.1:8080|" $service_path_dev
-sed -i -e "s|tendenci.access.log|dev-tendenci.access.log|g" $service_path_dev
-sed -i -e "s|tendenci.error.log|dev-tendenci.error.log|g" $service_path_dev
-sed -i -e 's|"tendenci"|"dev-tendenci"|g' $service_path_dev
+# Why do this when its in a separate folder? In case logs are aggregated.
+sed -i -e "s|tendenci.access.log|tendenci$file_suffix_dev.access.log|g" $service_path_dev
+sed -i -e "s|tendenci.error.log|tendenci$file_suffix_dev.error.log|g" $service_path_dev
+sed -i -e 's|"tendenci"|"tendenci"|g' $service_path_dev
 
 systemctl daemon-reload
 
@@ -50,7 +54,7 @@ rsync -a $env_path_prod/ $env_path_dev
 if ! [ 0 -eq $? ]; then echo "$env_path_dev wasn't created."; exit 1; fi
 
 find $env_path_dev -name '*.pyc' -delete
-# Hacky equivalent of virtualenv --relocable, but actually works for my use case
+# Hacky equivalent of virtualenv --relocatable, but actually works for my use case
 for file_path in $(grep -l -r "$env_path_prod" $env_path_dev); do
 	sed -i -e "s|$env_path_prod|$env_path_dev|g" $file_path
 done
@@ -70,25 +74,23 @@ su -c "createdb --owner=tendenci $db_name_dev" postgres
 su -c "pg_dump $db_name_prod | psql --quiet -o /dev/null $db_name_dev" postgres
 if ! [ 0 -eq $? ]; then echo "$db_name_dev wasn't created."; exit 1; fi
 
-# Delete settings which should not match
-sed -i -e "/^DATABASES.*$db_name_prod.*/d" $website_path_dev/conf/settings.py
-sed -i -e '/^SECRET_KEY.*/d' $website_path_dev/conf/settings.py
-sed -i -e '/^SITE_SETTINGS_KEY.*/d' $website_path_dev/conf/settings.py
-sed -i -e '/^EMAIL_.*/d' $website_path_dev/conf/settings.py
-sed -i -e '/^NEWSLETTER_EMAIL_.*/d' $website_path_dev/conf/settings.py
-
-# TODO: Change payment gateway to test endpoint
-cat << "EOT" >> $website_path_dev/conf/settings.py
-DATABASES['default']['NAME'] = 'tendenci_dev'
-SECRET_KEY=''
-SITE_SETTINGS_KEY=''
-EMAIL_BACKEND = 'django_log_email.backends.EmailBackend'
-EMAIL_LOG_FILE = '/srv/tendenci/log/email.log'
-SITE_CACHE_KEY = ''
-CACHE_PRE_KEY = SITE_CACHE_KEY
-set_app_log_filename('/srv/tendenci/logs/dev-app.log')
-set_debug_log_filename('/srv/tendenci/logs/dev-debug.log')
-# Populate with required domains if encountering 403's on login
-# CSRF_TRUSTED_ORIGINS = []
-EOT
+# TODO: add paypal overrides when known
+# Update settings
+sed -i -e "s/^# DEBUG = True/DEBUG = True/" $website_path_dev/conf/settings.py
+sed -i -e "s/^DATABASES.*$db_name_prod.*/DATABASES['default']['NAME'] = '$db_name_dev'/" $website_path_dev/conf/settings.py
+sed -i -e "s/^SECRET_KEY.*/SECRET_KEY='$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)'/" $website_path_dev/conf/settings.py
+# sed -i -e "s/^SITE_SETTINGS_KEY.*/SITE_SETTINGS_KEY='$(cat /dev/urandom | tr -dc 'a-zA-Z0-9' | fold -w 64 | head -n 1)'/" $website_path_dev/conf/settings.py
+sed -i -e "s/^EMAIL_BACKEND.*/EMAIL_BACKEND = 'django_log_email.backends.EmailBackend'/" $website_path_dev/conf/settings.py
+sed -i -e "s/^CELERY_EMAIL_BACKEND.*/CELERY_EMAIL_BACKEND = 'django_log_email.backends.EmailBackend'/" $website_path_dev/conf/settings.py
+sed -i -e "s|^EMAIL_LOG_FILE.*|EMAIL_LOG_FILE = '$log_path_dev/email$file_suffix_dev.log'|" $website_path_dev/conf/settings.py
+sed -i -e "/^EMAIL_LOG_BACKEND.*/d" $website_path_dev/conf/settings.py
+sed -i -e "/^NEWSLETTER_EMAIL_.*/d" $website_path_dev/conf/settings.py
+sed -i -e "s/^STRIPE_SECRET_KEY.*/STRIPE_SECRET_KEY = 'sk_test_[blah blah]'/" $website_path_dev/conf/settings.py
+sed -i -e "s/^STRIPE_PUBLISHABLE_KEY.*/STRIPE_PUBLISHABLE_KEY = 'pk_test_[blah blah]'/" $website_path_dev/conf/settings.py
+sed -i -e "s/^SITE_CACHE_KEY.*/SITE_CACHE_KEY = 'tendenci$file_suffix_dev'/" $website_path_dev/conf/settings.py
+sed -i -e "s|^set_app_log_filename.*|set_app_log_filename('$log_path_dev/app$file_suffix_dev.log')|" $website_path_dev/conf/settings.py
+sed -i -e "s|^set_debug_log_filename.*|set_debug_log_filename('$log_path_dev/debug$file_suffix_dev.log')|" $website_path_dev/conf/settings.py
+sed -i -e "s/^CSRF_TRUSTED_ORIGINS.*/CSRF_TRUSTED_ORIGINS = ['byo list of domains']/" $website_path_dev/conf/settings.py
+sed -i -e "s/^ALLOWED_HOSTS.*/ALLOWED_HOSTS = ['byo list of domains']/" $website_path_dev/conf/settings.py
+sed -i -e "s/^SERVER_EMAIL.*/SERVER_EMAIL = '$db_name_dev@localhost'/"  $website_path_dev/conf/settings.py
 
